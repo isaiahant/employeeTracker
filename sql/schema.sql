36011afba213e0aa4bcca065b596011056f229c4
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-13 11:49:30 -0800

    initial db and nodes setup

---
+++ sql/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department id INT NOT NULL
+);
+
+CREATE TABLE employee(
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first name VARCHAR(30) NOT NULL,
+  last name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT 
+);
