630af05d4e06e23471c8e84a4843deb1191b7f2a
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-15 15:51:17 -0800

    class constructors, inquirer prompt in progress

--- app.js
+++ app.js
@@ -2,9 +2,56 @@ const mysql = require('mysql')
 const inquirer = require('inquirer')
 const db = mysql.createConnection('mysql://root:Holytoledo16@localhost/employee_db')
 
-
-db.query('SELECT * FROM employee'), (err, employee) =>{
+let depart = []
+let roll = []
+let emp = []
+db.query('SELECT * FROM employee', (err, data) =>{
 if (err) { console.log(err) }
 
-console.log(employee)
+console.log(data)
+})
+
+
+function Menu() {
+  inquirer.prompt([
+    {
+      type:'list',
+      name:'menu',
+      message:'What would you like to do?',
+      choices:['Add', 'View', 'Update']
+    }
+  ])
+  .then(({menu}) =>{
+    switch (menu) {
+      case Add:
+        inquirer.prompt([
+          {
+            type:'list',
+            name:'adding',
+            message:'What would you like to add?',
+            choices:['Department', 'Role', 'Employee']
+          }
+        ])
+        .then(({adding})=>{
+          switch (adding) {
+            case Department:
+              inquirer.prompt([
+                {
+                  type:'input',
+                  name:'depName',
+                  message:'What is the name of the department?'
+                }
+              ])
+              break;
+          
+            default:
+              break;
+          }
+        })
+        break;
+    
+      default:
+        break;
+    }
+  })
 }
